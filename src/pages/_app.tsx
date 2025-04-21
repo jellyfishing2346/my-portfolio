f85a66c1add5e1310db01f1b1a34dba09f1a632a
jellyfishing2346 <108194702+jellyfishing2346@users.noreply.github.com>
2025-04-21 15:01:07 -0400

    fix: add default export to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
 // src/pages/_app.tsx
 import { ThemeProvider } from "next-themes";
-
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
